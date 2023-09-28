a7013084d7f05f28ec54616ef7c8994b6c43612e
perezsuarez <135983644+perezsuarez@users.noreply.github.com>
2023-09-28 10:47:11 -0300

    Add files via upload

---
+++ galeria_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:10,classNames:'vlightbox1',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,pauseLocation:'imageContainerMain',prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
